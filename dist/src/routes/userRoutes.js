bdb997024a5641764e0a06f94b927e4f9592d674
Sebastián <scalquin@sb.cl>
2021-10-03 23:54:36 -0300

    delete node_modules dist

--- dist/src/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var userController_1 = require("../controllers/userController");
-var router = (0, express_1.Router)();
-router.post("/login", userController_1.loginUser);
-exports.default = router;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidXNlclJvdXRlcy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uLy4uL3NyYy9yb3V0ZXMvdXNlclJvdXRlcy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFBLG1DQUFpQztBQUNqQyxnRUFFdUM7QUFFdkMsSUFBTSxNQUFNLEdBQUcsSUFBQSxnQkFBTSxHQUFFLENBQUM7QUFFeEIsTUFBTSxDQUFDLElBQUksQ0FBQyxRQUFRLEVBQUUsMEJBQVMsQ0FBQyxDQUFDO0FBRWpDLGtCQUFlLE1BQU0sQ0FBQyJ9
